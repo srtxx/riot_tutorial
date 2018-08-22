09572016ebb68e30845b7fb29e8d2eb6aea8f9df
Suganuma <b1504550@planet.kanazawa-it.ac.jp>
2018-08-22 17:14:59 +0900

    riotことはじめ

---
+++ riot_practice/1st/js/coffee.js
@@ -0,0 +1,3 @@
+riot.tag2('coffee', '<h1>{name}</h1>', '', '', function(opts) {
+    this.name = '不要な複雑さにまみれた現在の潮流に「反旗」を翻す';
+});
---
+++ riot_practice/1st/tags/coffee.tag
@@ -0,0 +1,6 @@
+<coffee>
+  <h1>{name}</h1>
+  <script>
+    this.name = 'Riotの意味は暴動';
+  </script>
+</coffee>
